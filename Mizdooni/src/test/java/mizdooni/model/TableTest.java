a8d99b873f272cbb3e7f472fbbe6cb3a202daa1e
mobinahz <mobina.haghizadeh@ut.ac.ir>
2024-10-17 18:47:21 +0330

    Implement TableTest class

--- Mizdooni/src/test/java/mizdooni/model/TableTest.java
+++ Mizdooni/src/test/java/mizdooni/model/TableTest.java
@@ -1,4 +1,73 @@
 package mizdooni.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TableTest {
+    private Table table;
+    private Address address;
+    private Reservation reservation1;
+    private Reservation reservation2;
+
+    @BeforeEach
+    void setUp() {
+
+        table = new Table(1, 1, 4);
+        address = new Address("Iran", "Tehran", "Nelson-Mandela");
+        User user = new User("mobina", "12345", "mobinahz@gmail.com", null, User.Role.client);
+
+
+        Restaurant restaurant = new Restaurant(
+                "ChapChap",
+                user,
+                "Asian",
+                LocalTime.of(10, 0),
+                LocalTime.of(23, 0),
+                "Yummy Foods",
+                address,
+                "image1.jpg"
+        );
+
+        reservation1 = new Reservation(user, restaurant, table, LocalDateTime.of(2024, 10, 18, 19, 0));
+        reservation2 = new Reservation(user, restaurant, table, LocalDateTime.of(2024, 10, 19, 19, 0));
+    }
+
+    @Test
+    void testAddReservation() {
+        table.addReservation(reservation1);
+
+        List<Reservation> reservations = table.getReservations();
+        assertEquals(1, reservations.size());
+        assertEquals(reservation1, reservations.get(0));
+    }
+
+    @Test
+    void testIsReservedAtExactTime() {
+        table.addReservation(reservation1);
+
+        assertTrue(table.isReserved(LocalDateTime.of(2024, 10, 18, 19, 0)));
+    }
+
+    @Test
+    void testIsReservedAtDifferentTime() {
+        table.addReservation(reservation1);
+
+        assertFalse(table.isReserved(LocalDateTime.of(2024, 10, 18, 23, 0)));
+    }
+
+    @Test
+    void testIsReservedCancelled() {
+        table.addReservation(reservation1);
+        reservation1.cancel();
+
+        assertFalse(table.isReserved(reservation1.getDateTime()));
+    }
+
+
 }
